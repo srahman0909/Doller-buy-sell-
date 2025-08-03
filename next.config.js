ad8b2e920364169d4154c374543f6cd941b1e3ee
srahman0909 <jimmd8467@gmail.com>
2025-08-03 19:12:52 +0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
+};
+
+module.exports = nextConfig;
