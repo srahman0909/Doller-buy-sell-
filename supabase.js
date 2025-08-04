59dfd49daa6ec63ab65e173593a91e8865b0923e
srahman0909 <jimmd8467@gmail.com>
2025-08-04 22:51:00 +0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,7 @@
-// supabase.js
+
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
 const SUPABASE_URL = "https://piscmjpcvcgefxqohjki.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpc2NtanBjdmNnZWZ4cW9oamtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMzczMjIsImV4cCI6MjA2OTcxMzMyMn0.GIrT97QUl-C6BdooXAXUDVrxJKtHeHLikhX5HSk2UHA";
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
