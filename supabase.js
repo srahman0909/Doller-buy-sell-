20407f545378b8fff73b8eff4659a166b68daee1
srahman0909 <jimmd8467@gmail.com>
2025-08-04 14:46:15 +0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = 'https://piscmjpcvcgefxqohjki.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpc2NtanBjdmNnZWZ4cW9oamtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMzczMjIsImV4cCI6MjA2OTcxMzMyMn0.GIrT97QUl-C6BdooXAXUDVrxJKtHeHLikhX5HSk2UHA';
+const supabaseUrl = "https://piscmjpcvcgefxqohjki.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpc2NtanBjdmNnZWZ4cW9oamtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMzczMjIsImV4cCI6MjA2OTcxMzMyMn0.GIrT97QUl-C6BdooXAXUDVrxJKtHeHLikhX5HSk2UHA";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseKey);
