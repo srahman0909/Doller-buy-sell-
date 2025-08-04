d142c23622184187d6eb30244d89be854fde6ec5
srahman0909 <jimmd8467@gmail.com>
2025-08-04 10:47:07 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,1 @@
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  },
-};
-
-module.exports = nextConfig;
