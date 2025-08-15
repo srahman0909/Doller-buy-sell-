bb009d62f7843afc95113b49ad7bccbb5258f692
srahman0909 <jimmd8467@gmail.com>
2025-08-15 16:03:07 +0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,10 @@
 
-<script>
-  (function(){
-    const SUPABASE_URL = "https://piscmjpcvcgefxqohjki.supabase.co";
-    const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpc2NtanBjdmNnZWZ4cW9oamtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMzczMjIsImV4cCI6MjA2OTcxMzMyMn0.GIrT97QUl-C6BdooXAXUDVrxJKtHeHLikhX5HSk2UHA";
 
-    window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-  })();
-</script>
+import { createClient } from '@supabase/supabase-js';
+
+
+const SUPABASE_URL = "https://piscmjpcvcgefxqohjki.supabase.co";
+
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpc2NtanBjd mNnZWZ4cW9oamtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI2MDAwMDAsImV4cCI6MjAzODE3NjAwMH0.J9hHqSe5R-L5o5mXvwO6fCznPqf7LZlqgCJQAvP0Ywo";
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
